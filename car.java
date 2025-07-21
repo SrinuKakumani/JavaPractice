4b2e57ca3e6bfcbbadb107febe6046e8f79f3bd0
SrinuKakumani <srinivaskakumani614@gmail.com>
2025-07-21 14:30:01 +0530

    Practice

---
+++ car.java
@@ -0,0 +1,29 @@
+public class car {
+     public static void main(String[] args){
+
+        String Engine_type = "mHAWK 4 Cylinder";
+        String Dispacement = "2184 cc";
+        String Max_Power = "130bhp@3750rpm";
+        String Max_torque = "300Nm@1600-2800rpm";
+        byte No_of_cylinders = 4;
+        byte Valves_per_Cylinder = 4;
+        String Fuel_Supply_System = "CRDi";
+        boolean Turbo_charger = true;
+        String Transmission_Type = "Manual";
+        String Gearbox = "6-speed";
+        String Drive_Type = "RWD";
+        
+        System.out.println("Engine_type:"+Engine_type);
+        System.out.println("Dispacement:"+Dispacement);
+        System.out.println("Max_Power:"+Max_Power);
+        System.out.println("Max_torque:"+Max_torque);
+        System.out.println("No_of_cylinders:"+No_of_cylinders);
+        System.out.println("Valves_per_Cylinder:"+Valves_per_Cylinder);
+        System.out.println("Fuel_Supply_System:"+Fuel_Supply_System);
+        System.out.println("Turbo_charger:"+Turbo_charger);
+        System.out.println("Transmission_Type:"+Transmission_Type);
+        System.out.println("Gearbox:"+Gearbox);
+        System.out.println("Drive_Type:"+Drive_Type);
+
+     }
+}
