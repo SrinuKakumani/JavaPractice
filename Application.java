cf34b1e32589fe6653572e8225a5d58276f26731
SrinuKakumani <srinivaskakumani614@gmail.com>
2025-07-21 13:52:47 +0530

    java prctice

--- Application.java
+++ Application.java
@@ -1,3 +1,79 @@
 public class Application {
-    
+    public static void main(String[] args){
+
+        String candidateName = "Srinu Kakumani";
+        String fatherName = "Kakumani Subba Rao";
+        String motherName = "Kakumani Rameswari";
+        String category = "GEN"; // GEN / OBC / SC / ST / Minority / Others
+        String fatherOccupation = "Farmer";
+        double annualIncome = 100000.00;
+        String address = "1-2-3/3/4,Kakumani Vari Palem";
+        String post = "Kandukur";
+        String district = "Nellore";
+        int pincode = 523105;
+        String state = "Andhra Pradesh";
+        String studentMobile = "8309582973";
+        String parentMobile = "8309582973";
+        String email = "srinivaskakumani614@gmail.com";
+       
+        String domicileState = "Andhra Pradesh";
+
+        String branchName = "ECE";
+
+        String aadharNumber = "9566 3567 6543";
+
+        String secondaryBoard = "BIEAP";
+        int secondaryYear = 2019;
+        String secondarySchool = "Sri Chaitanya";
+        double secondaryCGPA = 9.8;
+        double secondaryTotalMarks = 576;
+        double secondaryPercentage = 98.0;
+
+        String srSecondaryBoard = "BIEAP";
+        int srSecondaryYear = 2021;
+        String srSecondarySchool = "Sri Chaitanya";
+        double srSecondaryCGPA = 9.0;
+        double srSecondaryTotalMarks = 906;
+        double srSecondaryPercentage = 90.6;
+
+        System.out.println("candidate Name: " + candidateName);
+        System.out.println("Father's Name: " + fatherName);
+        System.out.println("Mother's Name: " + motherName);
+        System.out.println("Category: " + category);
+        System.out.println("Father's Occupation: " + fatherOccupation);
+        System.out.println("Annual Income: " + annualIncome);
+        System.out.println("Address: " + address);
+        System.out.println("Post: " + post);
+        System.out.println("City: " + city);
+        System.out.println("District: " + district);
+        System.out.println("Pincode: " + pincode);
+        System.out.println("State: " + state);
+        System.out.println("Student Mobile: " + studentMobile);
+        System.out.println("Parent Mobile: " + parentMobile);
+        System.out.println("Email: " + email);
+        System.out.println("Post: " + post);
+        System.out.println("Is Resident Of Pilani: " + isResidentOfpilani);
+        System.out.println("Studied In Birla Schools: " + studiedInBirlaSchools);
+        System.out.println("Domicile State: " + domicileState);
+        System.out.println("JEE Roll No.: " + jeeRollNo);
+        System.out.println("JEE Marks: " + jeeMarks);
+        System.out.println("JEE Rank: " + jeeRank);
+        System.out.println("Branch Preference: " + branchName);
+        System.out.println("Aadhar Number: " + aadharNumber);
+        System.out.println("Secondary Board: " + secondaryBoard);
+        System.out.println("Secondary Year: " + secondaryYear);
+        System.out.println("Secondary School: " + secondarySchool);
+        System.out.println("Secondary CGPA: " + secondaryCGPA);
+        System.out.println("Secondary Total Marks: " + secondaryTotalMarks);
+        System.out.println("Secondary Percentage: " + secondaryPercentage);
+
+        System.out.println("Sr Secondary Board: " + srSecondaryBoard);
+        System.out.println("Sr Secondary Year: " + srSecondaryYear);
+        System.out.println("Sr Secondary School: " + srSecondarySchool);
+        System.out.println("Sr Secondary CGPA: " + srSecondaryCGPA);
+        System.out.println("Sr Secondary Total Marks: " + srSecondaryTotalMarks);
+        System.out.println("Sr Secondary Percentage: " + srSecondaryPercentage);
+       
+
+    }
 }
